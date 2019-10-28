4f8e7541baba563c3e0c2cf6e142a9f7ddf528dd
Ville Saukkonen <villesau@msn.com>
2019-10-28 18:25:12 +0200

    module not found issue

--- js2/test.js
+++ js2/test.js
@@ -1,3 +1,7 @@
 // @flow
+import * as redux from 'redux'; // redux not found?
+import test from '../js/test2';
 
-import test from 'js/test2';
+test(123); // should error since the fn does not take arguments
+
+redux.createStore();
